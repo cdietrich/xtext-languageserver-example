1e367e89ffb18b3cb3fe8e2a1921629abc405d59
Christian Dietrich <christian.dietrich@itemis.de>
2016-11-04 11:17:00 +0100

    use spaces instead of tabs
    Signed-off-by: Christian Dietrich <christian.dietrich@itemis.de>

--- vscode-extension-self-contained/src/extension.ts
+++ vscode-extension-self-contained/src/extension.ts
@@ -7,8 +7,8 @@ import { workspace, ExtensionContext } from 'vscode';
 import { LanguageClient, LanguageClientOptions, ServerOptions } from 'vscode-languageclient';
 
 export function activate(context: ExtensionContext) {
-	// The server is a locally installed self contained jar
-	let jar = context.asAbsolutePath(path.join('src', 'mydsl-full.jar'));
+    // The server is a locally installed self contained jar
+    let jar = context.asAbsolutePath(path.join('src', 'mydsl-full.jar'));
     
     let serverOptions: ServerOptions = {
         run : { command: "java", args:['-jar', jar] },
@@ -21,14 +21,14 @@ export function activate(context: ExtensionContext) {
             fileEvents: workspace.createFileSystemWatcher('**/*.*')
         }
     };
-	
-	// Create the language client and start the client.
+    
+    // Create the language client and start the client.
     let lc = new LanguageClient('Xtext Server', serverOptions, clientOptions);
     // enable tracing (.Off, .Messages, Verbose)
     lc.trace = Trace.Verbose;
-	let disposable = lc.start();
-	
-	// Push the disposable to the context's subscriptions so that the 
-	// client can be deactivated on extension deactivation
-	context.subscriptions.push(disposable);
+    let disposable = lc.start();
+    
+    // Push the disposable to the context's subscriptions so that the 
+    // client can be deactivated on extension deactivation
+    context.subscriptions.push(disposable);
 }
