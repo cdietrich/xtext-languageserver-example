db92016cf2f3fd16f76a1b9a258a240b12cbc961
Holger Schill <Holger.Schill@itemis.de>
2017-01-16 17:35:20 +0100

    Use AsynchronousServerSocketChannel for socket communication.

--- org.xtext.example.mydsl.ide/src/org/xtext/example/mydsl/ide/RunServer.java
+++ org.xtext.example.mydsl.ide/src/org/xtext/example/mydsl/ide/RunServer.java
@@ -1,6 +1,12 @@
 package org.xtext.example.mydsl.ide;
 import java.io.IOException;
 import java.net.InetSocketAddress;
+import java.net.SocketAddress;
+import java.nio.channels.AsynchronousServerSocketChannel;
+import java.nio.channels.AsynchronousSocketChannel;
+import java.nio.channels.Channels;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import java.util.function.Function;
@@ -26,12 +32,24 @@ public class RunServer {
 			MessageConsumer result = consumer;
 			return result;
 		};
-		Launcher<LanguageClient> launcher = Launcher.createSocketLauncher(languageServer, LanguageClient.class, new InetSocketAddress("localhost", 5007), Executors.newCachedThreadPool(), wrapper);
+		Launcher<LanguageClient> launcher = createSocketLauncher(languageServer, LanguageClient.class, new InetSocketAddress("localhost", 5007), Executors.newCachedThreadPool(), wrapper);
 		languageServer.connect(launcher.getRemoteProxy());
 		Future<?> future = launcher.startListening();
 		while (!future.isDone()) {
 			Thread.sleep(10_000l);
 		}
 	}
+	
+    static <T> Launcher<T> createSocketLauncher(Object localService, Class<T> remoteInterface, SocketAddress socketAddress, ExecutorService executorService, Function<MessageConsumer, MessageConsumer> wrapper) throws IOException {
+        AsynchronousServerSocketChannel serverSocket = AsynchronousServerSocketChannel.open().bind(socketAddress);
+        AsynchronousSocketChannel socketChannel;
+        try {
+            socketChannel = serverSocket.accept().get();
+            return Launcher.createIoLauncher(localService, remoteInterface, Channels.newInputStream(socketChannel), Channels.newOutputStream(socketChannel), executorService, wrapper);
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 
 }
