c23fece5fc2ea3a4f24d1b1aacfdcdebb416a147
Christian Dietrich <christian.dietrich@itemis.de>
2023-01-13 10:55:01 +0100

    ditch travis
    Signed-off-by: Christian Dietrich <christian.dietrich@itemis.de>

--- .travis-publishOnRelease.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Execute only on tag builds where the tag starts with 'v'
-
-if [[ -n "$TRAVIS_TAG" && "$TRAVIS_TAG" == v* ]]; then
-    echo "Publishing version: $TRAVIS_TAG"
-    ./gradlew publish
-fi
